e16db14e05dcda46eff41d63fb68911effd2552a
Ankita-CCI <ankita@creativecapsule.com>
2014-07-28 12:09:19 +0530

    initial commit of storyboard

---
+++ BTLEDemoApp/BTLEDemoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BTLEDemoApp
+//
+//  Created by Ankita Kalangutkar on 28/07/14.
+//  Copyright (c) 2014 creative capsule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BTLEDemoApp/BTLEDemoApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BTLEDemoApp
+//
+//  Created by Ankita Kalangutkar on 28/07/14.
+//  Copyright (c) 2014 creative capsule. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
